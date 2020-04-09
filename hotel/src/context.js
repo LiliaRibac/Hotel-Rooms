484d94adaf1cbc2607860767f9d453f27c954116
LiliaRibac <ribac.lilia@gmai.com>
2020-04-09 13:35:05 -0700

    add method handleChange in context file and able to filter room type

--- hotel/src/context.js
+++ hotel/src/context.js
@@ -8,17 +8,32 @@ class RoomProvider extends Component {
     sortedRooms: [],
     featuredRooms: [],
     loading: true,
+    type:"all",
+    capacity: 1,
+    price: 0,
+    minPrice: 0,
+    maxPrice: 0,
+    minSize: 0,
+    maxSize: 0,
+    breakfast: false,
+    pets: false,
   };
   // getData
   componentDidMount() {
     let rooms = this.formatData(items);
-    let featuredRooms = rooms.filter(room =>room.featured === true);
+    let featuredRooms = rooms.filter((room) => room.featured === true);
+    let maxPrice = Math.max(...rooms.map((item) => item.price));
+    let maxSize = Math.max(...rooms.map((item) => item.size));
+
     this.setState({
       rooms,
       featuredRooms,
-      sortedRooms:rooms,
-      loading:false
-    })
+      sortedRooms: rooms,
+      loading: false,
+      maxPrice: maxPrice,
+      maxPrice,
+      maxSize,
+    });
   }
 
   formatData(newItems) {
@@ -31,16 +46,26 @@ class RoomProvider extends Component {
     });
     return tempItems;
   }
-  
-getRoom = slug =>{
-  let tempRooms =[...this.state.rooms]
-   let room = tempRooms.find(item =>item.slug ===slug)
-   return room
-}
 
+  getRoom = (slug) => {
+    let tempRooms = [...this.state.rooms];
+    let room = tempRooms.find((item) => item.slug === slug);
+    return room;
+  };
+
+  handleChange = event =>{
+    const target = event.target
+    const value= event.type === "checkbox" ? target.checked : target.value;
+    const name = event.target.name
+    this.setState({[name]:value},this.filterRooms)
+  }
+
+  filterRooms =() =>{
+    console.log("hello")
+  }
   render() {
     return (
-      <RoomContext.Provider value={{ ...this.state, getRoom: this.getRoom }}>
+      <RoomContext.Provider value={{ ...this.state, getRoom: this.getRoom, handleChange:this.handleChange }}>
         {this.props.children}
       </RoomContext.Provider>
     );
